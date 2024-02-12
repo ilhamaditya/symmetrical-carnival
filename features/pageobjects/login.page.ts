7ea980c2ace0f3d91cee39d0ebc0fedf0a034016
ilhamaditya <adi3tz.ophelia@gmail.com>
2024-02-12 14:10:01 +0700

    End to end login flow

--- features/pageobjects/login.page.ts
+++ features/pageobjects/login.page.ts
@@ -6,15 +6,23 @@ class LoginPage {
     }
 
     get email () {
-        return $('');
+        return $('//android.widget.EditText[@content-desc="input-email"]');
     }
 
     get password () {
-        return $('');
+        return $('//android.widget.EditText[@content-desc="input-password"]');
     }
 
     get btnLogin () {
-        return $('');
+        return $('//android.view.ViewGroup[@content-desc="button-LOGIN"]/android.view.ViewGroup/android.widget.TextView');
+    }
+    
+    get message () {
+        return $('//android.widget.TextView[contains(@text, "You are logged in!")]')
+    }
+
+    get btnOk () {
+        return $('//android.widget.Button[contains(@text, "OK")]')
     }
 
     async login (email: string, password: string) {
--- features/step-definitions/login.steps.ts
+++ features/step-definitions/login.steps.ts
@@ -1,18 +1,19 @@
-import { Given, When, Then } from '@wdio/cucumber-framework';
-import HomePage from '../pageobjects/home.page.ts'; // Adjust the path accordingly
+import { Given, When, Then } from "@wdio/cucumber-framework";
+import HomePage from "../pageobjects/home.page.ts"; // Adjust the path accordingly
+import LoginPage from "../pageobjects/login.page.ts";
+import { expect } from 'chai';
 
-
-Given(/^I am on home page$/, async () => {
-});
+Given(/^I am on home page$/, async () => {});
 
 When(/^I navigate to Login$/, async () => {
-    await HomePage.navigateToLogin()
+  await HomePage.navigateToLogin();
 });
 
-When(/^input <email> and <password>$/, () => {
-    return true;
+When(/^input "([^"]*)" and "([^"]*)"$/, async (email, password) => {
+  await LoginPage.login(email, password);
 });
 
-Then(/^I should see success modal <message>$/, () => {
-    return true;
+Then(/^I should see success modal <message>$/, async () => {
+ await expect(LoginPage.message).exist;
+ (await LoginPage.btnOk).click()
 });
